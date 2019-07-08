fb18a2e7bb2d444f98b31b825af927a9f4bd998a
Thao Do <thaodo.momo@gmail.com>
2019-07-08 10:09:33 +0900

    ok

--- FirstBarcode/src/main/java/Zxing1.java
+++ FirstBarcode/src/main/java/Zxing1.java
@@ -9,42 +9,45 @@ import java.io.File;
 import java.io.IOException;
 
 public class Zxing1 {
-    public static void main(String[] args) {
-        File folder = new File("src/resource/EAN13");
-        File[] listOfFiles = folder.listFiles();
-        for (int i = 0; i < listOfFiles.length; i++) {
-            File file = listOfFiles[i];
-            BufferedImage image = null;
-            BinaryBitmap bitmap = null;
-            Result result = null;
 
-            try {
-                image = ImageIO.read(file);
-                System.out.println("Before resize: width-" + image.getWidth() + "|height-" + image.getHeight());
+  public static void main(String[] args) {
+    File folder = new File("FirstBarcode/src/resource/EAN13");
+    File[] listOfFiles = folder.listFiles();
+    for (int i = 0; i < listOfFiles.length; i++) {
+      File file = listOfFiles[i];
+      BufferedImage image = null;
+      BinaryBitmap bitmap = null;
+      Result result = null;
+
+      try {
+        image = ImageIO.read(file);
+//                System.out.println("Before resize: width-" + image.getWidth() + "|height-" + image.getHeight());
 //            Dimension newMaxSize = new Dimension(220, 160);
 //            image = Scalr.resize(image, Scalr.Method.QUALITY, newMaxSize.getWidth(), newMaxSize.getHeight());
-                System.out.println("After resize: width-" + image.getWidth() + "|height-" + image.getHeight());
+//                System.out.println("After resize: width-" + image.getWidth() + "|height-" + image.getHeight());
 
-                int[] pixels = image.getRGB(0, 0, image.getWidth(), image.getHeight(), null, 0, image.getWidth());
-                RGBLuminanceSource source = new RGBLuminanceSource(image.getWidth(), image.getHeight(), pixels);
-                bitmap = new BinaryBitmap(new HybridBinarizer(source));
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
+        int[] pixels = image
+            .getRGB(0, 0, image.getWidth(), image.getHeight(), null, 0, image.getWidth());
+        RGBLuminanceSource source = new RGBLuminanceSource(image.getWidth(), image.getHeight(),
+            pixels);
+        bitmap = new BinaryBitmap(new HybridBinarizer(source));
+      } catch (IOException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+      }
 
-            EAN13Reader reader = new EAN13Reader();
-            System.out.print(file.getName()+": ");
-            try {
-                result = reader.decode(bitmap);
-                System.out.println(result.getText());
-            } catch (NotFoundException e) {
-                e.printStackTrace();
-            } catch (FormatException e) {
-                e.printStackTrace();
-            }
-        }
-
+      EAN13Reader reader = new EAN13Reader();
+      System.out.print(file.getName() + ": ");
+      try {
+        result = reader.decode(bitmap);
+        System.out.println(result.getText());
+      } catch (NotFoundException e) {
+        System.out.println(e.getMessage());
+      } catch (FormatException e) {
+        System.out.println(e.getMessage());
+      }
     }
+
+  }
 
 }
