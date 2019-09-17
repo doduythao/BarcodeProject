95f4384eb11569118cd9f755f4b1eb94707f2282
Thao <doduythao@gmail.com>
2019-09-17 13:01:23 +0900

    8

--- matlab/cvpr16_make_nonuniform_blury/run_synthesize_nonuniform.m
+++ matlab/cvpr16_make_nonuniform_blury/run_synthesize_nonuniform.m
@@ -1,6 +1,6 @@
 
-input_dir = fullfile('dataset', 'input');
-output_dir  = fullfile('output', 'nonuniform');
+input_dir = 'D:/temp/';
+output_dir  = 'D:/temp_out/';
 if( ~exist(output_dir, 'dir') )
     mkdir(output_dir);
 end
@@ -8,26 +8,22 @@ end
 gyro_dir = fullfile('dataset', 'gyro');
 noise = 0.01;
 
-img_list = load_list('list/input.txt');
+img_list = dir(fullfile(input_dir,'*.png'));
 gyro_list = load_list('list/gyro.txt');
 
 for i = 1:length(img_list)
-    img_name = sprintf('%s.png', img_list{i});
-    img_path = fullfile(input_dir, img_name);
     
-    fprintf('Load %s\n', img_path);
-    img = imread(img_path);
+    img_name = img_list(i).name;
+    full_path = fullfile(input_dir, img_name);
+    [ ~, sname, ~] = fileparts(full_path);
+    img = imread(full_path);
 
-    for g = 1:length(gyro_list)
-
-        gyro_filename = fullfile(gyro_dir, gyro_list{g}, 'gyro.txt');
+    parfor g = 1:length(gyro_list)
         info_filename = fullfile(gyro_dir, gyro_list{g}, 'info.txt');
+        gyro_filename = fullfile(gyro_dir, gyro_list{g}, 'gyro.txt');
 
         % read trajectory and camera info
-        fprintf('Load %s\n', gyro_filename);
         info_data = importdata(info_filename);
-
-        fprintf('Load %s\n', info_filename);
         gyro_data = importdata(gyro_filename);
 
         % synthesize non-uniform blur from 6D camera trajectory
@@ -36,9 +32,9 @@ for i = 1:length(img_list)
         % add noise
         blur_img = blur_img + noise * rand(size(blur_img));
 
-        output_name = sprintf('%s_%s.png', img_list{i}, gyro_list{g});
+        output_name = sprintf('%s-%s.png', sname, gyro_list{g});
         output_path = fullfile(output_dir, output_name);
-        fprintf('Save %s\n', output_path);
+        % fprintf('Save %s\n', output_path);
         imwrite(blur_img, output_path);
     end
 end
