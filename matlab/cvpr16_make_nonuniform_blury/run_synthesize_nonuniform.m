24da61b1d9bf8b8c7a10525253f07d728739c3e9
Thao Do <13520797@gm.uit.edu.vn>
2019-09-18 22:17:19 +0900

    9

--- matlab/cvpr16_make_nonuniform_blury/run_synthesize_nonuniform.m
+++ matlab/cvpr16_make_nonuniform_blury/run_synthesize_nonuniform.m
@@ -1,20 +1,25 @@
 
-input_dir = 'D:/temp/';
-output_dir  = 'D:/temp_out/';
+org_dir = 'C:/Users/thao/DATA/org1';
+inp_dir = 'C:/Users/thao/DATA/inp1';
+if( ~exist(inp_dir, 'dir') )
+    mkdir(inp_dir);
+end
+output_dir  = 'C:/Users/thao/DATA/out1';
 if( ~exist(output_dir, 'dir') )
     mkdir(output_dir);
 end
 
+
 gyro_dir = fullfile('dataset', 'gyro');
 noise = 0.01;
 
-img_list = dir(fullfile(input_dir,'*.png'));
+img_list = dir(fullfile(org_dir,'*.png'));
 gyro_list = load_list('list/gyro.txt');
 
 for i = 1:length(img_list)
     
     img_name = img_list(i).name;
-    full_path = fullfile(input_dir, img_name);
+    full_path = fullfile(org_dir, img_name);
     [ ~, sname, ~] = fileparts(full_path);
     img = imread(full_path);
 
@@ -34,7 +39,9 @@ for i = 1:length(img_list)
 
         output_name = sprintf('%s-%s.png', sname, gyro_list{g});
         output_path = fullfile(output_dir, output_name);
+        input_path = fullfile(inp_dir, output_name);
         % fprintf('Save %s\n', output_path);
         imwrite(blur_img, output_path);
+        imwrite(img, input_path);
     end
 end
