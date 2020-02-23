f0e73895973db988f4a4832b072af56858e71ed6
doduythao <doduythao@gmail.com>
2020-02-23 19:25:59 +0900

    mobilenet_norm

---
+++ mobilenet_norm/model.py
@@ -0,0 +1,86 @@
+import glob
+import os
+
+import torch
+import torch.nn as nn
+import torchvision.models as models
+
+
+class Model(nn.Module):
+    CHECKPOINT_FILENAME_PATTERN = 'model-{}.pth'
+
+    __constants__ = ['mobilenet',
+                     'fc', 'avgpool',
+                     '_digit1', '_digit2', '_digit3', '_digit4', '_digit5', '_digit6', '_digit7', '_digit8', '_digit9',
+                     '_digit10', '_digit11', '_digit12', '_digit13']
+
+    def __init__(self):
+        super(Model, self).__init__()
+        self.mobilenet = models.mobilenet_v2(pretrained=False).features
+        
+        self._digit1 = nn.Sequential(
+            nn.Linear(1280*9*9, 10, bias=True))
+        self._digit2 = nn.Sequential(
+            nn.Linear(1280*9*9, 10, bias=True))
+        self._digit3 = nn.Sequential(
+            nn.Linear(1280*9*9, 10, bias=True))
+        self._digit4 = nn.Sequential(
+            nn.Linear(1280*9*9, 10, bias=True))
+        self._digit5 = nn.Sequential(
+            nn.Linear(1280*9*9, 10, bias=True))
+        self._digit6 = nn.Sequential(
+            nn.Linear(1280*9*9, 10, bias=True))
+        self._digit7 = nn.Sequential(
+            nn.Linear(1280*9*9, 10, bias=True))
+        self._digit8 = nn.Sequential(
+            nn.Linear(1280*9*9, 10, bias=True))
+        self._digit9 = nn.Sequential(
+            nn.Linear(1280*9*9, 10, bias=True))
+        self._digit10 = nn.Sequential(
+            nn.Linear(1280*9*9, 10, bias=True))
+        self._digit11 = nn.Sequential(
+            nn.Linear(1280*9*9, 10, bias=True))
+        self._digit12 = nn.Sequential(
+            nn.Linear(1280*9*9, 10, bias=True))
+        self._digit13 = nn.Sequential(
+            nn.Linear(1280*9*9, 10, bias=True))
+
+    def forward(self, x):
+        x = self.mobilenet(x)
+        # x = self.avgpool(x)
+#         print(x.shape)
+        x = x.reshape(x.size(0), -1)
+        # x = self.fc(x)
+
+        digit1_logits = self._digit1(x)
+        digit2_logits = self._digit2(x)
+        digit3_logits = self._digit3(x)
+        digit4_logits = self._digit4(x)
+        digit5_logits = self._digit5(x)
+        digit6_logits = self._digit6(x)
+        digit7_logits = self._digit7(x)
+        digit8_logits = self._digit8(x)
+        digit9_logits = self._digit9(x)
+        digit10_logits = self._digit10(x)
+        digit11_logits = self._digit11(x)
+        digit12_logits = self._digit12(x)
+        digit13_logits = self._digit13(x)
+
+        return digit1_logits, digit2_logits, digit3_logits, digit4_logits, digit5_logits, digit6_logits, digit7_logits, \
+               digit8_logits, digit9_logits, digit10_logits, digit11_logits, digit12_logits, digit13_logits
+
+    def store(self, path_to_dir, step, maximum=5):
+        path_to_models = glob.glob(os.path.join(path_to_dir, Model.CHECKPOINT_FILENAME_PATTERN.format('*')))
+        if len(path_to_models) == maximum:
+            min_step = min([int(path_to_model.split('/')[-1][6:-4]) for path_to_model in path_to_models])
+            path_to_min_step_model = os.path.join(path_to_dir, Model.CHECKPOINT_FILENAME_PATTERN.format(min_step))
+            os.remove(path_to_min_step_model)
+
+        path_to_checkpoint_file = os.path.join(path_to_dir, Model.CHECKPOINT_FILENAME_PATTERN.format(step))
+        torch.save(self.state_dict(), path_to_checkpoint_file)
+        return path_to_checkpoint_file
+
+    def restore(self, path_to_checkpoint_file):
+        self.load_state_dict(torch.load(path_to_checkpoint_file))
+        step = int(path_to_checkpoint_file.split('/')[-1][6:-4])
+        return step
---
+++ mobilenet_norm/train.py
@@ -0,0 +1,166 @@
+import argparse
+import os
+import time
+from datetime import datetime
+
+import numpy as np
+import torch
+torch.manual_seed(0)
+
+import torch.nn.functional
+import torch.optim as optim
+import torch.utils.data
+from torch.optim.lr_scheduler import StepLR
+from torchvision import transforms
+
+from dataset import BarcodeDataset
+from evaluator import Evaluator
+from model import Model
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-d', '--data_dir', default='./data', help='directory to read LMDB files')
+parser.add_argument('-l', '--logdir', default='./logs', help='directory to write logs')
+parser.add_argument('-r', '--restore_checkpoint', default=None,
+                    help='path to restore checkpoint, e.g. ./logs/model-100.pth')
+parser.add_argument('-bs', '--batch_size', default=32, type=int, help='Default 32')
+parser.add_argument('-lr', '--learning_rate', default=7e-3, type=float, help='Default 7e-2')
+parser.add_argument('-p', '--patience', default=100, type=int, help='Default 100, set -1 to train infinitely')
+parser.add_argument('-ds', '--decay_steps', default=10000, type=int, help='Default 10000')
+parser.add_argument('-dr', '--decay_rate', default=0.95, type=float, help='Default 0.95')
+parser.add_argument('-tf', '--train_files', default='../model/data/syn_train.txt', help='directory to train list file')
+parser.add_argument('-vf', '--val_files', default='../model/data/real_full.txt', help='directory to validation list file')
+
+
+def _loss(digit1_logits, digit2_logits, digit3_logits, digit4_logits, digit5_logits, digit6_logits, digit7_logits, digit8_logits, digit9_logits, digit10_logits, digit11_logits, digit12_logits, digit13_logits, digits_labels):
+    digit1_cross_entropy = torch.nn.functional.cross_entropy(digit1_logits, digits_labels[0])
+    digit2_cross_entropy = torch.nn.functional.cross_entropy(digit2_logits, digits_labels[1])
+    digit3_cross_entropy = torch.nn.functional.cross_entropy(digit3_logits, digits_labels[2])
+    digit4_cross_entropy = torch.nn.functional.cross_entropy(digit4_logits, digits_labels[3])
+    digit5_cross_entropy = torch.nn.functional.cross_entropy(digit5_logits, digits_labels[4])
+    digit6_cross_entropy = torch.nn.functional.cross_entropy(digit6_logits, digits_labels[5])
+    digit7_cross_entropy = torch.nn.functional.cross_entropy(digit7_logits, digits_labels[6])
+    digit8_cross_entropy = torch.nn.functional.cross_entropy(digit8_logits, digits_labels[7])
+    digit9_cross_entropy = torch.nn.functional.cross_entropy(digit9_logits, digits_labels[8])
+    digit10_cross_entropy = torch.nn.functional.cross_entropy(digit10_logits, digits_labels[9])
+    digit11_cross_entropy = torch.nn.functional.cross_entropy(digit11_logits, digits_labels[10])
+    digit12_cross_entropy = torch.nn.functional.cross_entropy(digit12_logits, digits_labels[11])
+    digit13_cross_entropy = torch.nn.functional.cross_entropy(digit13_logits, digits_labels[12])
+    loss = digit1_cross_entropy + digit2_cross_entropy + digit3_cross_entropy + digit4_cross_entropy \
+           + digit5_cross_entropy + digit6_cross_entropy + digit7_cross_entropy + digit8_cross_entropy \
+           + digit9_cross_entropy + digit10_cross_entropy + digit11_cross_entropy + digit12_cross_entropy \
+           + digit13_cross_entropy
+    return loss
+
+
+def _train(train_img_path, train_txt_path, val_img_path, val_txt_path, path_to_log_dir,
+           path_to_restore_checkpoint_file, training_options):
+    batch_size = training_options['batch_size']
+    initial_learning_rate = training_options['learning_rate']
+    initial_patience = training_options['patience']
+    num_steps_to_show_loss = 100
+    num_steps_to_check = 1000
+
+    step = 0
+    patience = initial_patience
+    best_accuracy = 0.0
+    duration = 0.0
+
+    model = Model()
+    model.cuda()
+
+    transform = transforms.Compose([
+                transforms.Resize([285, 285]),
+                transforms.ToTensor(),
+                transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+            ])
+    train_loader = torch.utils.data.DataLoader(BarcodeDataset(training_options['train_files'], train_img_path, train_txt_path, transform),
+                                               batch_size=batch_size, shuffle=True,
+                                               num_workers=4, pin_memory=True)
+    evaluator = Evaluator(training_options['val_files'], val_img_path, val_txt_path)
+    optimizer = optim.SGD(model.parameters(), lr=initial_learning_rate, momentum=0.9, weight_decay=0.0005)
+    scheduler = StepLR(optimizer, step_size=training_options['decay_steps'], gamma=training_options['decay_rate'])
+
+    if path_to_restore_checkpoint_file is not None:
+        assert os.path.isfile(path_to_restore_checkpoint_file), '%s not found' % path_to_restore_checkpoint_file
+        step = model.restore(path_to_restore_checkpoint_file)
+        scheduler.last_epoch = step
+        print('Model restored from file: %s' % path_to_restore_checkpoint_file)
+
+    path_to_losses_npy_file = os.path.join(path_to_log_dir, 'losses.npy')
+    if os.path.isfile(path_to_losses_npy_file):
+        losses = np.load(path_to_losses_npy_file)
+    else:
+        losses = np.empty([0], dtype=np.float32)
+
+    while True:
+        for batch_idx, (images, digits_labels) in enumerate(train_loader):
+            start_time = time.time()
+            images, digits_labels = images.cuda(), [digit_label.cuda() for digit_label in digits_labels]
+            digit1_logits, digit2_logits, digit3_logits, digit4_logits, digit5_logits, digit6_logits, digit7_logits, digit8_logits, digit9_logits, digit10_logits, digit11_logits, digit12_logits, digit13_logits = model.train()(images)
+            loss = _loss(digit1_logits, digit2_logits, digit3_logits, digit4_logits, digit5_logits, digit6_logits, digit7_logits, digit8_logits, digit9_logits, digit10_logits, digit11_logits, digit12_logits, digit13_logits, digits_labels)
+            optimizer.zero_grad()
+            loss.backward()
+
+#             torch.nn.utils.clip_grad_norm_(model.parameters(), 3.0)
+            optimizer.step()
+            scheduler.step()
+            step += 1
+            duration += time.time() - start_time
+
+            if step % num_steps_to_show_loss == 0:
+                examples_per_sec = batch_size * num_steps_to_show_loss / duration
+                duration = 0.0
+                print('=> %s: step %d, loss = %f, learning_rate = %f (%.1f examples/sec)' % (
+                    datetime.now(), step, loss.item(), scheduler.get_lr()[0], examples_per_sec))
+
+            if step % num_steps_to_check != 0:
+                continue
+
+            losses = np.append(losses, loss.item())
+            np.save(path_to_losses_npy_file, losses)
+
+            print('=> Evaluating on validation dataset...')
+            accuracy = evaluator.evaluate(model)
+            print('==> accuracy = %f, best accuracy %f' % (accuracy, best_accuracy))
+
+            if accuracy > best_accuracy:
+                path_to_checkpoint_file = model.store(path_to_log_dir, step=step)
+                print('=> Model saved to file: %s' % path_to_checkpoint_file)
+                patience = initial_patience
+                best_accuracy = accuracy
+            else:
+                patience -= 1
+
+            print('=> patience = %d' % patience)
+            if patience == 0:
+                return
+
+
+def main(args):
+    train_img_path ='../model/data/syn_train/img/'
+    train_txt_path ='../model/data/syn_train/gt/'
+    val_img_path   ='../model/data/real/img/'
+    val_txt_path   ='../model/data/real/txt/'
+    path_to_log_dir = args.logdir
+    path_to_restore_checkpoint_file = args.restore_checkpoint
+    training_options = {
+        'batch_size': args.batch_size,
+        'learning_rate': args.learning_rate,
+        'patience': args.patience,
+        'decay_steps': args.decay_steps,
+        'decay_rate': args.decay_rate,
+        'train_files': args.train_files,
+        'val_files': args.val_files
+    }
+
+    if not os.path.exists(path_to_log_dir):
+        os.makedirs(path_to_log_dir)
+
+    print('Start training')
+    _train(train_img_path, train_txt_path, val_img_path, val_txt_path, path_to_log_dir,
+           path_to_restore_checkpoint_file, training_options)
+    print('Done')
+
+
+if __name__ == '__main__':
+    main(parser.parse_args())
